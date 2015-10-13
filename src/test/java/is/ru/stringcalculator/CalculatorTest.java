7a1a188b56b41378c6b2810a677ec74cddcd253d
Karl Ingi Karlsson <karlk13@i-7f3b5dd9.csd07cloud.internal>
2015-10-13 22:23:34 +0000

    Added test: testMoreMultipleNumbers()

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -28,5 +28,9 @@ public class CalculatorTest {
     public void testMultipleNumbers(){
     	assertEquals(6, Calculator.add("1,2,3"));
     }
-
+    
+    @Test
+    public void testMoreMultipleNumbers(){
+        assertEquals(45, Calculator.add("1,2,3,4,5,6,7,8,9"));
+    }
 }
