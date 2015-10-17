a8d235089fc965944fd049675326a592bf3d1604
Karl Ingi Karlsson <karlk13@i-7f3b5dd9.csd07cloud.internal>
2015-10-17 19:21:42 +0000

    Added support for long delimiters and a corresponding test

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -10,15 +10,33 @@ public class Calculator {
 		}
         else {            
             if(text.startsWith("//")){
-                char delimiter = findDelimiter(text);
-                String subText = text.substring(4);
-                return sum(splitNumbers(subText, delimiter));
+                if (text.charAt(2) == '[') {
+                    int count = findInputStart(text);
+                    System.out.println("Count: " + count);
+                    String delimiter = findStringDelimiter(text);
+                    String subText = text.substring(count);
+                    return sum(splitNumbers(subText, delimiter));    
+                }
+                else {
+                    char delimiter = findDelimiter(text);
+                    String subText = text.substring(4);
+                    return sum(splitNumbers(subText, delimiter));
+                }
             }
 		    if(text.contains(",") || text.contains("\n")){
 			    return sum(splitNumbers(text));
 		    }
 		return 1;
 	    }
+    }
+
+    private static int findInputStart(String text) {
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == '\n') {
+                return i + 1;
+            }
+        }
+        return 0;
     }
 
 	private static int toInt(String number){
@@ -34,8 +52,47 @@ public class Calculator {
         return numbers.split(regSplit);
 	}
     
+	private static String[] splitNumbers(String numbers, String delimiter){
+        String regSplit = ",|\n|" + delimiter;
+        return numbers.split(regSplit);
+	}
+
     private static char findDelimiter(String text){
         return text.charAt(2);
+    }
+ 
+    private static String findStringDelimiter(String text){
+        String delimiter = "";
+        boolean add = false;
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == '[') {
+                add = true;
+                continue;
+            }
+            if (text.charAt(i) == ']') {
+                add = false;
+                break;
+            }
+            if (add) {
+                if (isLegal(text.charAt(i))) {
+                    delimiter = delimiter + text.charAt(i);
+                }
+                else {
+                    delimiter = delimiter + "\\" + text.charAt(i);
+                }
+            }   
+        }
+        return delimiter;
+    }
+
+    public static boolean isLegal(char c){
+        if  (c == '\\' || c == '.' || c == '[' || c == ']' ||
+            c == '{' || c == '}' || c == '(' || c == ')' ||
+            c == '*' || c == '+' || c == '-' || c == '?' ||
+            c == '^' || c == '$' || c == '|'){
+                return false;
+        }
+        return true;
     }
 
     private static int sum(String[] numbers){
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -63,4 +63,10 @@ public class CalculatorTest {
     public void testNumbersOver1000() {
         assertEquals(2, Calculator.add("1001,2"));
     }
+
+    @Test
+    public void testLongerDelimiters() {
+        assertEquals(6, Calculator.add("//[***]\n1***2***3"));
+    }
+
 }
